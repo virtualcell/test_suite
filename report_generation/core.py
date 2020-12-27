3560da2b856f095e73f7f8f7e346c7852aca23bc
GMarupilla <marupilla@mail.com>
2020-12-27 13:09:12 -0500

    Upload artifacts

--- report_generation/core.py
+++ report_generation/core.py
@@ -2,20 +2,24 @@ from report_generation.combine.omex_maker import GenOmex
 from report_generation.sbml import download_sbml
 from report_generation.sedml.sedml_maker import gen_sedml
 from report_generation.comparator.comparator import gen_report
-import os
 import subprocess
 
 download_sbml()
+
 print("Strating to generate SED-ML")
 gen_sedml()
 print("SED-ML generation finished")
+
 omex = GenOmex()
 omex.omex_gen_copasi()
 omex.omex_gen_vcell()
 
 
-list_files = subprocess.run(["./report_generation/run_script/run_copasi_sim.sh"])
-print("The exit code was: %d" % list_files.returncode)
-list_files = subprocess.run(["./report_generation/run_script/run_vcell_sim.sh"])
-print("The exit code was: %d" % list_files.returncode)
+copasi_run = subprocess.run(["./report_generation/run_script/run_copasi_sim.sh"])
+print("The exit code was: %d" % copasi_run.returncode)
+
+vcell_run = subprocess.run(
+    ["./report_generation/run_script/run_vcell_sim.sh"])
+print("The exit code was: %d" % vcell_run.returncode)
+
 gen_report()
