90a2e3a0257520b96daa14fd8d640b243c316f71
GMarupilla <marupilla@mail.com>
2021-01-05 14:40:34 -0500

    Reorganise comparator

--- report_generation/core.py
+++ report_generation/core.py
@@ -1,7 +1,7 @@
 from report_generation.combine.omex_maker import GenOmex
 from report_generation.sbml import download_sbml
 from report_generation.sedml.sedml_maker import gen_sedml
-from report_generation.comparator.comparator import gen_report
+from report_generation.comparator.report_comparator import ReportComparator
 import subprocess
 from logzero import logger
 
@@ -10,7 +10,8 @@ from logzero import logger
 
 try:
     logger.debug("Download starting...")
-    download_sbml(2, 0, -1)
+    # TODO: Make this configuration acceptable from command line interface
+    download_sbml(1000, 0, -1)
     logger.debug("Download finished...\n\n")
 
     logger.debug("Starting to generate SED-ML documents...")
@@ -24,19 +25,19 @@ try:
 
     # TODO: Replace with run_docker.py
     logger.debug("Running COPASI simulations...")
-    copasi_run = subprocess.run(["./report_generation/run_script/run_copasi_sim.sh"])
+    copasi_run = subprocess.run(
+        ["./report_generation/run_script/run_copasi_sim.sh"])
     logger.info("The exit code was: %d" % copasi_run.returncode)
     logger.debug("COPASI simulations finished...\n\n")
 
-
     logger.debug("Running VCell simulations...")
     vcell_run = subprocess.run(
-        ["./report_generation/run_script/run_vcell_sim.sh"], stdout=subprocess.PIPE, text=True, check=True)
+        ["./report_generation/run_script/run_vcell_sim.sh"])
     logger.info("The exit code was: %d" % vcell_run.returncode)
     logger.debug("VCell simulations finished...\n\n")
 
     logger.debug("Generating report...")
-    gen_report()
+    ReportComparator().generate_report()
     logger.debug("Report generated...")
 
 except KeyboardInterrupt:
